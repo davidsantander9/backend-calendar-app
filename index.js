6a1ab4f31277e42f6c1f4d761fcaa5bae4c78faa
David <davidsantander9@gmail.com>
2021-07-24 17:31:54 -0500

    Ãconfig express

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello world!');
+const express = require('express');
+
+// create express server
+
+const app = express();
+
+
+// Routes
+app.get('/', (req, res) => {
+    console.log('/')
+    res.json({
+        ok: true
+    })
+});
+
+// Listen request
+app.listen( 4000, () =>{
+    console.log(`Hello Server ${ 4000 }`)
+} )
