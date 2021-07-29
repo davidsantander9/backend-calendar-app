710f5daf28e43b722e7db025abe3319da2e4ba96
David <davidsantander9@gmail.com>
2021-07-29 14:16:38 -0500

    add react-app

---
+++ public/precache-manifest.cd5d22a2294648e23193cb35375e91d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3622c27084b75e0cc5df38e4edc16112",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04638f3313c707e17829",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "9cc864498cfb132d671b",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "04638f3313c707e17829",
+    "url": "/static/js/2.7c8777c6.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.7c8777c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc864498cfb132d671b",
+    "url": "/static/js/main.dfb70292.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
